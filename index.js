593fa576f75c602554364c41c45949ede9556e2c
Robert Valek <rvalek@intersog.com>
2019-04-07 19:36:09 +0300

    more minor cleaning

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const robert = require('./lib/robert');
 
 
 (() => {
-  // const inputAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  // const inputAlphabet = 'abcdefghijklmnopqrstuvwxyz';
   const inputAlphabet = ['a', 'b', 'c'];
   const alpaDKAs = dkasForAlpabet(inputAlphabet);
   const system = robert(alpaDKAs);
